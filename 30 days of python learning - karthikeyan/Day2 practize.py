5650f482309d19a83d10bc227daa581db7f351a1
karthikeyanramar06-star <karthikeyanramar06@gmail.com>
2026-06-20 19:28:00 +0530

    Day 1 and Day 2 practice files

---
+++ 30 days of python learning - karthikeyan/Day2 practize.py
@@ -0,0 +1,54 @@
+#personal information
+Name,county,gender,age,married= "karthikeyan","india","Male",20,"No"
+print("First name:",Name)
+print("Country name:",county)
+print("Gender:",gender)
+print("Age:",age)
+print("Marital status:",married)
+
+#built in functions and variables
+Name = input("What is your name?")
+country = input("where are you from")
+Age = input("What is your age?")
+print(Name)
+print(country)
+print(Age)
+
+#variable.py practize
+first_name="Karthikeyan"
+last_name="Ramar"
+full_name="Karthikeyan Ramar"
+country="India"
+city="coimbatore"
+age=20
+year = 2026
+is_married="False"
+is_true="True"
+is_light_on="True"
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on
+
+#excercise 2
+type=type(first_name)
+print(type)
+print(type(age))
+print(type(full_name))
+length=len("karthikeyan")
+print(length)
+length=len("Ramar")
+print(length)
+num1=int(5)
+num2=int(4)
+total=num1+num2
+print(total)
+difference=num1-num2
+print(difference)
+product=num1*num2
+print(product)
+division=num1/num2
+print(division)
+remainder=num1%num2
+print(remainder)
+exp=num1^num2
+print(exp)
+floor_division=num1//num2
+print(floor_division)
---
+++ 30 days of python learning - karthikeyan/Day2.py
@@ -0,0 +1,25 @@
+#built in functions
+print("hello world")
+print(len("hello world"))
+print(type("hello world"))
+print(str(90))
+print(float(56))
+print(max([20,30,45]))
+print(min([45,9,99]))
+print(sum([23,43,54]))
+
+#personal information
+Name,county,gender,age,married= "karthikeyan","india","Male",20,"No"
+print("First name:",Name)
+print("Country name:",county)
+print("Gender:",gender)
+print("Age:",age)
+print("Marital status:",married)
+
+#built in functions and variables
+Name = input("What is your name?")
+country = input("where are you from")
+Age = input("What is your age?")
+print(Name)
+print(country)
+print(Age)
