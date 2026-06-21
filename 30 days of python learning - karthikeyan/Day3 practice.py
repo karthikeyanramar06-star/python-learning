194b2409cbf7e0e390cc2ffc4ac8c41023559c23
karthikeyanramar06-star <karthikeyanramar06@gmail.com>
2026-06-21 20:19:54 +0530

    Day 3

---
+++ 30 days of python learning - karthikeyan/Day3 practice.py
@@ -0,0 +1,29 @@
+Age = int(20)
+print(Age)
+Height = float(18.0)
+print(Height)
+Nahuh =(1+6J)
+print(Nahuh)
+Base = float(input("Enter Base: "))
+Height = float(input("Enter Height: "))
+area_of_triangle = 0.5*Base*Height
+print("Area of triangle:",area_of_triangle)
+#perimeter of triangle
+side_a = float(input("Enter side a:"))
+side_b=float(input("Enter side b:"))
+side_c=float(input("Enter side c:"))
+print("Perimeter of triangle:",side_a+side_b+side_c)
+#area of rectangle
+l=float(input("enter length:"))
+b=float(input("enter breadth:"))
+area_of_rectangle=l*b
+print("Area of rectangle:",area_of_rectangle)
+perimeter_of_rectangle=2*(l+b)
+print("Perimeter of rectangle:",perimeter_of_rectangle)
+#radius & circumference of circle
+r=float(input("Enter radius:"))
+pi=3.14
+area_of_circle=pi*r**2
+print("Area of circle:",area_of_circle)
+circumference_of_circle=2*pi*r
+print("Circumference of circle:",circumference_of_circle)
---
+++ 30 days of python learning - karthikeyan/Day3.py
@@ -0,0 +1,42 @@
+#assignment operators 
+a=3
+b=4
+c=6
+d=7
+a+=2
+b-=4
+c*=3
+d/=9
+print(a)
+print(b)
+print(c)
+print(d)
+
+# arithemetic operators
+a-=10
+b+=15
+print(a)
+print(b)
+
+#calculation of area of circle,
+#area of rectangle
+#weight of an object
+#Density of a liquid
+
+radius = 20
+pi = 3.14
+area_of_circle= pi*radius**2
+print('Area of circle:',area_of_circle)
+length = 20
+breadth = 10
+area_of_rectangle=length*breadth
+print("area of rectangle",area_of_rectangle)
+mass = 20
+gravity =9.81
+weight_of_an_object=mass*gravity
+print("Mass of an object:",weight_of_an_object)
+mass=20
+volume=40
+density_of_a_liquid=mass/volume
+print("Density of an volume:",density_of_a_liquid)
+       
