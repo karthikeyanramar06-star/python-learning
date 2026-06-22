99f4bd8ccd7f8a87dbbe9650a1d7c6f57d5340db
karthikeyanramar06-star <karthikeyanramar06@gmail.com>
2026-06-22 20:51:29 +0530

    Day4

--- week1/madlib.py
+++ week1/madlib.py
@@ -2,3 +2,13 @@ noun = input("Give me a noun: ")
 verb = input("Give me a verb: ")
 adjective = input("Give me an adjective: ")
 print(f"The {adjective} {noun} decided to {verb} all day long.")
+
+#Area of the house
+def area (length,width):
+    return length*width
+
+def main():
+    House = (50,50)
+    Yard = (20,70)
+    total_sq = House * Yard
+    print(str(total_sq) + "square feet")
