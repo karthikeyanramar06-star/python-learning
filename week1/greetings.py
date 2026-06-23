bf82c7034ba03a9009c9719d1e2f1f33d7581731
karthikeyanramar06-star <karthikeyanramar06@gmail.com>
2026-06-23 19:29:34 +0530

    Intial Changes

--- week1/greetings.py
+++
@@ -1,3 +0,0 @@
-name = input("What is your name? ")
-age = int(input("How old are you? "))
-print(f"Hi {name}, you will be {age + 1} next year.")
--- week1/hello name.py
+++
@@ -1,4 +0,0 @@
-name = input("what is your name?")
-name = name.strip()
-name = name.title()
-print("hey " , name ,sep="nice to meet you ",end=". ")
